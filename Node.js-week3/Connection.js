e5e9bd0dbdc8f2252a0482eb11bea5a3ac634bd5
Wameedh <38753468+Wameedh112@users.noreply.github.com>
2018-09-06 21:09:49 +0200

    Node.js-week3

---
+++ Node.js-week3/Connection.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+
+const databaseConfiguration = { 
+   host: "sql7.freemysqlhosting.net",
+   user: "sql7254337",
+   password: process.env.password,
+   database:"sql7254337"
+}
+
+const connection = mysql.createConnection(databaseConfiguration);
+
+connection.connect((err)=> {
+   if (err) {
+     console.error('error connecting: ' + err.stack);
+     return;
+   }
+
+   console.log('connected as id ' + connection.threadId);
+ });
+
+ module.exports = {
+     connection
+ }
+
---
+++ Node.js-week3/index.js
@@ -0,0 +1,203 @@
+const express = require("express");
+const server = express();
+const PORT = process.env.PORT || 8000;
+const  bodyParser = require("body-parser");
+const cors = require("cors");
+const {connection } = require("./Connection");
+const fs = require("fs");
+const uuidv4 = require('uuid/v4');
+const compression = require('compression');
+const helmet = require('helmet');
+const {SHA256,AES, enc} =require("crypto-js");
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on Localhost:${PORT}`);
+ });
+
+ server.use(express.static('public'));
+ server.use(bodyParser.json());
+ server.use(
+     cors({
+         oringin:"http://localhost:3000"
+        })
+    );
+ server.use(compression());
+ server.use(helmet());
+ server.get("/",(request, response)=>{
+     response.send("on the master branch");
+    });
+    
+
+//registration*************************************
+server.get("/registration/:username/:password",(request, response)=> {
+    const{ username, password:stupidPassword } = request.params;
+    //const stupidPassword= "wameedh112";
+    const hasedpassword = SHA256(stupidPassword).toString();
+    const salt = uuidv4();
+    const encrytpassword = AES.encrypt(hasedpassword,salt).toString();
+    const sql = `INSERT INTO user SET ?`;
+    const values ={
+        username,
+        password: encrytpassword,
+        salt
+    }
+    connection.query(sql, values,(error, results)=>{
+        if(error){ showError(error, response);
+        }
+        response.json({status: "success" , message : "registered"})
+    });
+ });
+//login********************************************
+server.get("/login/:username/:password",(request, response)=> {
+    const {username, password} = request.params;
+    const hasedpassword = SHA256(password).toString();
+    const sql = `SELECT * FROM user WHERE ?`;
+    const values = {username};
+    connection.query(sql, values, (error, results) => {
+        const dbPassword = results[0].password;
+        const Decryptpassword = AES.decrypt(dbPassword.toString(), results[0].salt);
+        const sqlPassword = Decryptpassword.toString(enc.Utf8);
+        if(error){ showError(error, response);
+        }
+        if (hasedpassword === sqlPassword) {
+            response.json({status: "success",  message: "You are logged in"});
+        } else {
+            response.json({status: "error",message: "Enter a valid username and password"});
+        }
+    });
+});
+  
+ //get jokes*******************************************
+ server.get("/get/jokes", (request ,response) => {
+    connection.query("SELECT * FROM joke order by id desc ", (error , results) => {
+        if (error){
+            showError(error);
+        }
+        response.json(results);
+    })
+});
+//post image****************************************
+server.post("/post/joke", (request, response) => {
+    const { body } = request;
+    if (body){
+        const { title , file} = body;
+        if(file){
+            const { base64 } = file;
+            const fileName = uuidv4();
+            fs.writeFile(`./public/images/${fileName}.jpeg`, base64, 'base64', (error) => {
+                if(error){
+                    console.log(error);
+                }
+            })
+            const sql = "INSERT into joke set ?";
+            const values ={
+                image_location: `/images/${fileName}.jpeg`,
+                title
+            }
+            connection.query(sql, values , (error, result) =>{
+                if(error){
+                    showError(error, response);
+                }else{
+                    console.log(body);
+                    response.json({
+                        status:"succes",
+                        message:"joke uploaded"
+                    })
+                }
+            })
+        }
+    }
+});
+
+//To get the data of commented image from joke table************************
+server.get('/get/joke/:id', (request, response) => {
+    const values = [request.params.id];
+    const sql = "SELECT * FROM joke where id = ?";
+    connection.query(sql, values, (error, results) => {
+        if (error) {
+            showError(error, response)
+        }
+        response.json(results[0]);
+    });
+});
+
+//comments**************************************************
+server.get(`/get/comments/:jokeId`, (request, response) => {
+    const {body} = request;
+    if (body) {
+        const sql = `SELECT * FROM comment where joke_id = ?`;
+        const values = [request.params.jokeId];
+        connection.query(sql, values, (error, results) => {
+            if (error) {
+                showError(error, response);
+            }
+            response.json(results);
+        });
+    }
+});
+
+//post comments*********************************
+server.post("/post/comment", (request, response) => {
+    const {body} = request;
+    if (body) {
+        const { text, username, joke_id} = body;
+        const sql ="INSERT into comment set ?";
+        const values ={
+            text: `${text}`,
+            username :`${username}`,
+            joke_id: `${joke_id}`
+        }
+        connection.query(sql, values, (error, results) => {
+            if (error) {
+          showError(error, response);
+        }
+        console.log(results);
+        response.json({ 
+            status: "succes",
+             message: "comment posted"
+            });
+        });
+    }
+});
+
+//:vote up and down************************************
+server.post("/update/joke/:vote",(request,response) => {
+    const voteType= request.params.vote;
+    const { body } = request;
+    if(body){
+        const { id } = body;
+        if(id){
+            vote(id , voteType , response);
+        }
+    }
+})
+  
+function vote(id , voteType , response ){
+    let sql;
+    if(voteType === "upvote"){
+        sql ="update joke set up_votes =up_votes + 1 where id = ? ";
+    }else{
+        sql ="update joke set down_votes = down_votes - 1 where id = ? ";
+    }
+    let values = [id];
+    connection.query(sql,values,(error,results)=>{
+        if(error){
+            showError(error,response);
+        }
+        console.log(results);
+        response.json({
+            status: "succes",
+            message: "joke voted"
+        })
+    })
+}
+
+//error**************************************
+function showError(error, response){
+    console.log (error);
+    response.json({
+        status: "error",
+        message:"something went worng"
+    });
+}
+//End Homework******************************************
